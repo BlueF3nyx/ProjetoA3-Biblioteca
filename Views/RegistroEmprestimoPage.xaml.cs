d009180e054866b53943bf748dcd29d7c3494d1f
Pedro Henrique <BlueF3ny>
2025-05-31 21:31:17 -0300

    criação do arquivo xaml.cs

---
+++ Views/RegistroEmprestimoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+
+namespace BibliotecaAPP.Views
+{
+    public partial class RegistroEmprestimoPage : Window
+    {
+        public class LivroEmprestado
+        {
+            public string? Livro { get; set; }
+            public int? Duracao { get; set; }
+            public DateTime? DataEmprestimo { get; set; }
+            public DateTime? DataDevolucao { get; set; }
+        }
+
+        
+    }
+}
